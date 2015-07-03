275ef6e676052e4406c05ac7b0e111cfb391de8b
Nefla <neflasan@gmail.com>
2015-07-03 13:43:39 -0300

    Drop mirror from API, change module focus
    Purpose of the module become more about load modules and mapping
    instead of creating tasks in gulp.

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 var fs = require('fs'),
     Path = require('path'),
     io = require('./lib/io'),
-    mirror = require('./src/mirror'),
     reflect = require('./src/reflect');
 
 var parent = module.parent,
     parentFile = parent.filename,
     parentDir = Path.dirname(parentFile);
-
-function mirrorApi(dir, gulp, opts){
-  dir = dir || '.';
-  dir = Path.resolve(parentDir, dir);
-  var folders = io.getFolders(dir);
-
-  if (folders.length === 0)
-    return mirror.mapFiles(dir, gulp, opts);
-  else
-    return mirror.mapFolders(dir, folders, gulp, opts);
-}
 
 function reflectApi(dir, opts){
   dir = dir || '.';
@@ -31,6 +19,5 @@ function reflectApi(dir, opts){
 }
 
 module.exports = {
-  mirror: mirrorApi,
   reflect: reflectApi
 };
