16c4a808ebcf2a18b46bbe159cb48cbd34e7ce14
Nefla <neflasan@gmail.com>
2015-07-03 12:11:58 -0300

    Refactor module API functions
    Drop anonymous function and instead use named functions to export at the
    end of the module definition.

--- index.js
+++ index.js
@@ -4,32 +4,33 @@ var fs = require('fs'),
     mirror = require('./src/mirror'),
     reflect = require('./src/reflect');
 
-var parent = module.parent;
-var parentFile = parent.filename;
-var parentDir = Path.dirname(parentFile);
+var parent = module.parent,
+    parentFile = parent.filename,
+    parentDir = Path.dirname(parentFile);
+
+function mirrorApi(dir, gulp, opts){
+  dir = dir || '.';
+  dir = Path.resolve(parentDir, dir);
+  var folders = io.getFolders(dir);
+
+  if (folders.length === 0)
+    return mirror.mapFiles(dir, gulp, opts);
+  else
+    return mirror.mapFolders(dir, folders, gulp, opts);
+}
+
+function reflectApi(dir, opts){
+  dir = dir || '.';
+  dir = Path.resolve(parentDir, dir);
+  var folders = io.getFolders(dir);
+
+  if (folders.length === 0)
+    return reflect.mapFiles(dir, opts);
+  else
+    return reflect.mapFolders(dir, folders, opts);
+}
 
 module.exports = {
-  mirror: function(dir, gulp, opts){
-    dir = dir || '.';
-    dir = Path.resolve(parentDir, dir);
-    var folders = io.getFolders(dir);
-
-    if (folders.length === 0)
-      return mirror.mapFiles(dir, gulp, opts);
-    else
-      return mirror.mapFolders(dir, folders, gulp, opts);
-  },
-
-  reflect: function(dir, opts){
-    dir = dir || '.';
-    dir = Path.resolve(parentDir, dir);
-    var folders = io.getFolders(dir);
-
-    if (folders.length === 0) {
-      return reflect.mapFiles(dir, opts);
-    }
-    else {
-      return reflect.mapFolders(dir, folders, opts);
-    }
-  }
+  mirror: mirrorApi,
+  reflect: reflectApi
 };
