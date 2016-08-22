8b6966130fbd671464036d1f91fa60faffc552f0
Nefla <neflasan@gmail.com>
2016-08-22 02:10:33 -0300

    Update index

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-var fs = require('fs'),
-    path = require('path'),
-    dir = require('./lib/dir'),
-    mapper = require('./src/mapper');
+var path = require('path'),
+    mapper = require('./src/mapper')
 
-module.exports = function(dirName, config){
+module.exports = function(dirname, config){
   var config = config || {}
-  dirName = dirName || '.';
-  dirName = path.resolve(path.dirname(module.parent.filename), dirName);
-  var folders = dir.folders(dirName);
-
-  if (folders.length === 0)
-    return mapper.mapFiles(dirName, config);
-  else
-    return mapper.mapFolders(dirName, folders, config);
+  dirname = dirname || '.'
+  dirname = path.resolve(path.dirname(module.parent.filename), dirname)
+  return mapper(dirname, config)
 }
